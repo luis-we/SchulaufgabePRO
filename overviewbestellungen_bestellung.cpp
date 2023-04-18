0638f7c742bb9e90763762b2b5a989c8a9694e20
Valentin <valentin@wuerfelmail.de>
2023-04-18 14:25:12 +0200

    added stack and customer id

--- overviewbestellungen_bestellung.cpp
+++ overviewbestellungen_bestellung.cpp
@@ -1,14 +1,28 @@
+// overviewbestellungen_bestellung.cpp
 #include "overviewbestellungen_bestellung.h"
 #include "ui_overviewbestellungen_bestellung.h"
 
-overviewbestellungen_bestellung::overviewbestellungen_bestellung(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::overviewbestellungen_bestellung)
+#include "mainwindow.h"
+
+
+OverviewBestellungen_Bestellung::OverviewBestellungen_Bestellung(int customerId) :
+    ui(new Ui::overviewbestellungen_bestellung),
+    m_customerId(customerId)
 {
     ui->setupUi(this);
+
+    // Den customerId im Label_ID anzeigen
+    ui->ID_Label->setText(QString::number(customerId));
 }
 
-overviewbestellungen_bestellung::~overviewbestellungen_bestellung()
+OverviewBestellungen_Bestellung::~OverviewBestellungen_Bestellung()
 {
     delete ui;
 }
+
+
+void OverviewBestellungen_Bestellung::on_back_clicked()
+{
+
+}
+
--- overviewbestellungen_bestellung.h
+++ overviewbestellungen_bestellung.h
@@ -7,16 +7,23 @@ namespace Ui {
 class overviewbestellungen_bestellung;
 }
 
-class overviewbestellungen_bestellung : public QWidget
+class OverviewBestellungen;
+
+class OverviewBestellungen_Bestellung : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit overviewbestellungen_bestellung(QWidget *parent = nullptr);
-    ~overviewbestellungen_bestellung();
+    explicit OverviewBestellungen_Bestellung(int customerId);
+    ~OverviewBestellungen_Bestellung();
+
+private slots:
+    void on_back_clicked();
 
 private:
     Ui::overviewbestellungen_bestellung *ui;
+    int m_customerId;
+    OverviewBestellungen *m_parent;
 };
 
 #endif // OVERVIEWBESTELLUNGEN_BESTELLUNG_H
