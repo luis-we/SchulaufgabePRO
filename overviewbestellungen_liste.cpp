aa00d118ddb152cce2c7691a95383d2b178ca243
Valentin <valentin@wuerfelmail.de>
2023-05-05 19:57:06 +0200

    updated query

--- overviewbestellungen_liste.cpp
+++ overviewbestellungen_liste.cpp
@@ -1,8 +1,9 @@
 #include "overviewbestellungen_liste.h"
+#include "qsqlerror.h"
 #include "ui_overviewbestellungen_liste.h"
 #include <QSqlQuery>
 #include <QMessageBox>
-
+#include <QSqlQueryModel>
 
 overviewbestellungen_liste::overviewbestellungen_liste(int customerId, QStackedWidget *stack, QWidget *parent) :
     QWidget(parent),
@@ -11,14 +12,41 @@ overviewbestellungen_liste::overviewbestellungen_liste(int customerId, QStackedW
     m_stack(stack)
 {
     ui->setupUi(this);
-    ui->order_list->clear(); // Löscht alle vorhandenen Inhalte
 
-    QSqlQuery query("SELECT b.ID_Bestellung, k.Name, k.Vorname, a.Artikelname, z.Menge FROM bestellungen b INNER JOIN kunden k ON b.ID_Kunde = k.ID_Kunde INNER JOIN zuordnung_bestellungen_artikel z ON b.ID_Bestellung = z.ID_Bestellung INNER JOIN artikel a ON z.ID_Artikel = a.Artikelnummer");
+    QSqlQueryModel *model = new QSqlQueryModel();
 
-    while (query.next()) {
-    QString order = "Bestellung #" + query.value("ID_Bestellung").toString() + ": " + query.value("Name").toString() + ", " + query.value("Vorname").toString() + " - " + query.value("Artikelname").toString() + " (" + query.value("Menge").toString() + ")";
-    ui->order_list->append(order); // Fügt die Bestellung als neue Zeile hinzu
+    QSqlQuery query;
+    query.prepare("SELECT b.Bestelldatum, a.Artikelname, z.Menge, a.Preis_Netto, "
+                  "z.Menge * a.Preis_Netto AS Gesamtpreis_Netto "
+                  "FROM bestellungen b "
+                  "INNER JOIN kunden k ON b.ID_Kunde = k.ID_Kunde "
+                  "INNER JOIN zuordnung_bestellungen_artikel z ON b.ID_Bestellung = z.ID_Bestellung "
+                  "INNER JOIN artikel a ON z.ID_Artikel = a.Artikelnummer "
+                  "WHERE b.ID_Kunde = :customerId "
+                  "ORDER BY b.Bestelldatum ASC, a.Artikelname");
+
+    query.bindValue(":customerId", customerId);
+    if (!query.exec()) {
+        QMessageBox::critical(this, "Fehler", "Fehler bei der Datenbankabfrage: " + query.lastError().text());
+        return;
     }
+    query.exec();
+
+    model->setQuery(std::move(query));
+    ui->order_tableView->setModel(model);
+    ui->order_tableView->resizeColumnsToContents();
+    int columnCount = model->columnCount();
+    int availableWidth = ui->order_tableView->viewport()->width();
+    int defaultColumnWidth = availableWidth / columnCount;
+
+    for (int i = 0; i < columnCount; ++i) {
+        ui->order_tableView->setColumnWidth(i, defaultColumnWidth);
+    }
+
+    ui->order_tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Interactive);
+    ui->order_tableView->horizontalHeader()->setMaximumSectionSize(availableWidth);
+
+
 }
 
 overviewbestellungen_liste::~overviewbestellungen_liste()
