02bb2e92554fca9aefc6d63dc3de5dfcc1a925bc
Luis Westphal <westphalluis@gmail.com>
2023-05-05 09:01:57 +0200

    added comments

--- overviewkunde.h
+++ overviewkunde.h
@@ -11,32 +11,29 @@
 using namespace std;
 
 namespace Ui {
-class OverviewKunde;
+    class OverviewKunde;
 }
 
 class OverviewKunde : public QWidget
 {
     Q_OBJECT
 
+//Deklaration von Konstruktor und Destruktor
 public:
     explicit OverviewKunde(MainWindow* parent = nullptr);
     ~OverviewKunde();
 
+//Deklaration der Slots
 private slots:
     void on_back_to_main_clicked();
-
     void on_save_clicked();
-
     void on_reset_clicked();
-
     void on_list_item_clicked(ListItem<Kunde>* item);
-
     void on_create_clicked();
-
     void on_remove_clicked();
-
     void on_ort_currentIndexChanged(int index);
 
+//Deklaration privater Funktionen und Variablen
 private:
     void LoadCustomers();
     void LoadCustomer(Kunde* customer);
