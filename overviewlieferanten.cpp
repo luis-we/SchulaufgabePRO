b534eba681438ac9fa28009486650cc7ab653185
Andre <dev@geisselbrecht.eu>
2023-05-06 18:22:37 +0200

    Schreiben der Fkt, Anpassen GUI, Fehlerbehebungen, etc

--- overviewlieferanten.cpp
+++ overviewlieferanten.cpp
@@ -2,6 +2,7 @@
 #include "ui_overviewlieferanten.h"
 #include "listitem.h"
 #include "qsqlerror.h"
+#include "lieferant.h"
 
 #include <QSqlQuery>
 #include <QMessageBox>
@@ -43,11 +44,144 @@ OverviewLieferanten::OverviewLieferanten(MainWindow* parent) : QWidget(parent), 
         this->layout()->addWidget(button);
         this->lieferanten->push_back(lieferant);
     }
+
+
+    //Laden der Anreden
+    LadeAnreden();
+
+    //Laden der Orte
+    LadeOrte();
+
+    //Laden der Lieferanten
+    LadeLieferanten();
 }
 
 OverviewLieferanten::~OverviewLieferanten()
 {
     delete ui; //Löscht UI wenn Fenster geschlossen wird
+}
+
+//Anrede wird geladen und in DropDown Menü angezeigt
+void OverviewLieferanten::LadeAnreden()
+{
+    QSqlQuery query;
+
+    //SQL Abfrage nach allen Anreden aus Tabelle
+    query.prepare("SELECT * FROM anrede");
+
+    //Prüft ob Fehler beim SQL Querry auftreten und gibt Fehler aus
+    if (!query.exec()) {
+        qDebug() << "Fehler bei der Suchen nach Anreden: " << query.lastError().text();
+        return;
+    }
+
+    //Löschen aller Einträge im Drop-Down Menü
+    while (query.next()) {
+        //Auslesen der Anrede und der ID
+        int anrede_id = query.value("ID_Anrede").toInt();
+        QString anrede = query.value("Anrede").toString();
+
+        //Hinzufügen zum DropDown
+        ui->textBrowser_anrede->addItem(anrede, anrede_id);
+    }
+}
+
+// Lade Lieferanten aus DB
+void OverviewLieferanten::LadeLieferanten()
+{
+    QSqlQuery query;
+
+    // SQL Abfragein Tab Lieferanten
+    query.prepare("SELECT * FROM lieferanten");
+
+    // Fehlerabfrage
+    if (!query.exec()) {
+        qDebug() << "Fehler bei der Suchen nach Lieferanren: " << query.lastError().text();
+        return;
+    }
+
+    // Query Auslesen und schreiben in Lieferanten
+    while (query.next()) {
+        //
+        lieferanten* lieferanten = new lieferant(
+            query.value("ID_Lieferant").toInt(),
+            query.value("Lieferantenname").toString(),
+            query.value("Anrede_Ansprechpartner").toInt(),
+            query.value("Ansprechpartner").toString(),
+            query.value("Telefon").toString(),
+            query.value("Straße").toString(),
+            query.value("Hausnummer").toString(),
+            query.value("ID_Ort").toInt(),
+        );
+
+        // Hinzufügen des Lieferanten in Tabelle
+        ErstelleLieferant(lieferant);
+    }
+}
+
+
+
+//Leeren des Forms
+void OverviewLieferanten::LeereForm() {
+    ui->textBrowser_anrede->setCurrentIndex(-1);
+    ui->textBrowser_lieferantenansprechpartner->clear();
+    ui->textBrowser_lieferantenhsnr->clear();
+    ui->textBrowser_lieferantenid->clear();
+    ui->textBrowser_lieferantenname->clear();
+    ui->textBrowser_lieferantenstrasse->clear();
+    ui->textBrowser_ort->setCurrentIndex(-1);
+    ui->textBrowser_plz->clear();
+    ui->textBrowser_lieferantentelefon->clear();
+}
+
+//Lade Orte aus DB -> DropDown
+void OverviewLieferanten::LadeOrte()
+{
+    QSqlQuery query;
+
+    //Select Orte
+    query.prepare("SELECT * FROM ort");
+
+    //Check Datenbank Abfrage
+    if (!query.exec()) {
+        qDebug() << "Fehler bei der Suchen nach Orten: " << query.lastError().text();
+        return;
+    }
+
+    //alle Orte auslesen
+    while (query.next()) {
+        //in Var schreiben
+        int ort_id = query.value("ID_Ort").toInt();
+        QString ort = query.value("Ort").toString();
+
+        //Hinzufügen des Ortes in das Drop-Down Menü
+        ui->textBrowser_ort->addItem(ort, ort_id);
+    }
+}
+
+//Erstellen eines Listeneintrags vom Typ Kunde
+ListItem<lieferant>* OverviewLieferanten::ErstelleLieferant(lieferant* lieferant)
+{
+    //Erstellung des Buttons für den Kunden
+    QPushButton* button = new QPushButton(this);
+
+    //Erstellung des Listeneintrages (Verknüpfung von Button und Kunde)
+    ListItem<lieferant>* lieferatenTeil = new ListItem<lieferant>(lieferant, button);
+
+    //Verbindung des On-Click Events des Buttons mit der Funktion zur übermittlung des Ausgewählten Kunden
+    connect(button, &QPushButton::clicked, [this, lieferatenTeil](){ this->on_list_item_clicked(lieferatenTeil); });
+
+    //Setzen des Button Textes auf den Vollständigen Namen des Kunden
+    button->setText(lieferant->getDisplayText());
+
+    //Hinzugügen des Buttons in das Layout der Kundenliste
+    this->m_layout->addWidget(button); //?????????????
+
+    //Hinzufügen des Listeneintrags in den Vector der Kundenliste
+    this->m_customers->push_back(lieferatenTeil);//?????????????
+
+    //Rückgabe des Listeneintrages
+    return lieferatenTeil;
 }
 
 void OverviewLieferanten::on_list_item_clicked(ListItem<QString>* item)
@@ -65,17 +199,17 @@ void OverviewLieferanten::on_pushButton_anlegen_clicked()
     //Button zum Anlegen eines neuen Lieferanten in der SQL Tabelle Lieferanten
     QSqlQuery query_insert; //SQL Query zum Einfügen eines neuen Lieferanten
     //Zusammenbauen der SQL Abfrage mit den Werten aus den Textfeldern
-    query_insert.prepare("INSERT INTO lieferanten (Name, Adresse, Telefonnummer, Email) VALUES (:Name, :Anrede, :NameAnsprechpartner, :Telefon, :Straße, :Hausnummer, :Ort, :ID)");
+    query_insert.prepare("INSERT INTO lieferanten (Lieferantenname, Anrede_Ansprechpartner, Ansprechpartner, Telefon, Straße, Hausnummer, ID_Ort) VALUES (:Lieferantenname, :Anrede_Ansprechpartner, :Ansprechpartner, :Telefon, :Straße, :Hausnummer, :ID_Ort)");
 
     //Binden der Werte aus den Textfeldern an die SQL Abfrage
-    query_insert.bindValue(":Name", ui->textBrowser_lieferantenname->toPlainText());
-    query_insert.bindValue(":Andrede", ui->textBrowser_lieferantenandrede->toPlainText());
-    query_insert.bindValue(":NameAnsprechpartner", ui->textBrowser_lieferantenansprechpartner->toPlainText());
+    query_insert.bindValue(":Lieferantenname", ui->textBrowser_lieferantenname->toPlainText());
+    query_insert.bindValue(":Anrede_Ansprechpartner", ui->textBrowser_anrede->currentText());
+    query_insert.bindValue(":Ansprechpartner", ui->textBrowser_lieferantenansprechpartner->toPlainText());
     query_insert.bindValue(":Telefon", ui->textBrowser_lieferantentelefon->toPlainText());
     query_insert.bindValue(":Straße", ui->textBrowser_lieferantenstrasse->toPlainText());
     query_insert.bindValue(":Hausnummer", ui->textBrowser_lieferantenhsnr->toPlainText());
-    query_insert.bindValue(":Ort", ui->textBrowser_lieferantenort->toPlainText());
-    query_insert.bindValue(":ID", ui->textBrowser_lieferantenid->toPlainText());
+    query_insert.bindValue(":ID_Ort", ui->textBrowser_ort->currentText());
+    //query_insert.bindValue(":ID_Lieferant", ui->textBrowser_lieferantenid->toPlainText());
 
     query_insert.exec(); //Führt Query aus
     //Prüft ob Fehler beim SQL Querry auftreten und gibt Fehler aus
@@ -89,17 +223,17 @@ void OverviewLieferanten::on_pushButton_2_bearbeiten_clicked()
 {
     //Funktion um einen gesuchten Lieferanten zu bearbeiten und wieder in die SQL Tabelle zurückschreiben
     QSqlQuery query_update; //SQL Query zum Updaten eines Lieferanten
-    query_update.prepare("UPDATE lieferanten SET Name = :Name, Anrede = :Anrede, NameAnsprechpartner = :NameAnsprechpartner, Telefon = :Telefon, Straße = :Straße, Hausnummer = :Hausnummer, Ort = :Ort WHERE id = :ID");
+    query_update.prepare("UPDATE lieferanten SET Lieferantenname = :Lieferantenname, Anrede_Ansprechpartner = :Anrede_Ansprechpartner, Ansprechpartner = :Ansprechpartner, Telefon = :Telefon, Straße = :Straße, Hausnummer = :Hausnummer, ID_Ort = :ID_Ort WHERE ID_Lieferant = :ID_Lieferant");
     
     //Binden der Werte aus den Textfeldern an die SQL Abfrage
-    query_update.bindValue(":Name", ui->textBrowser_lieferantenname->toPlainText());
-    query_update.bindValue(":Andrede", ui->textBrowser_lieferantenandrede->toPlainText());
-    query_update.bindValue(":NameAnsprechpartner", ui->textBrowser_lieferantenansprechpartner->toPlainText());
+    query_update.bindValue(":Lieferantenname", ui->textBrowser_lieferantenname->toPlainText());
+    query_update.bindValue(":Anrede_Ansprechpartner", ui->textBrowser_anrede->currentText());
+    query_update.bindValue(":Ansprechpartner", ui->textBrowser_lieferantenansprechpartner->toPlainText());
     query_update.bindValue(":Telefon", ui->textBrowser_lieferantentelefon->toPlainText());
     query_update.bindValue(":Straße", ui->textBrowser_lieferantenstrasse->toPlainText());
     query_update.bindValue(":Hausnummer", ui->textBrowser_lieferantenhsnr->toPlainText());
-    query_update.bindValue(":Ort", ui->textBrowser_lieferantenort->toPlainText());
-    query_update.bindValue(":ID", ui->textBrowser_lieferantenid->toPlainText());
+    query_update.bindValue(":ID_Ort", ui->textBrowser_ort->currentText());
+    //query_update.bindValue(":ID_Lieferant", ui->textBrowser_lieferantenid->toPlainText());
 
     query_update.exec(); //Führt Query aus
     //Prüft ob Fehler beim SQL Querry auftreten und gibt Fehler aus
@@ -113,9 +247,9 @@ void OverviewLieferanten::on_pushButton_3_loeschen_clicked()
 {
     //Button zum Löschen eines Lieferanten
     QSqlQuery query_delete;
-    query_delete.prepare("DELETE FROM lieferanten WHERE id = :ID");
+    query_delete.prepare("DELETE FROM lieferanten WHERE ID_Lieferant = :ID_Lieferant");
 
-    query_delete.bindValue(":ID", ui->textBrowser_lieferantenid->toPlainText());
+    query_delete.bindValue(":ID_Lieferant", ui->textBrowser_lieferantenid->toPlainText());
 
     query_delete.exec(); //Führt Query aus
     //Prüft ob Fehler beim SQL Querry auftreten und gibt Fehler aus
@@ -123,4 +257,31 @@ void OverviewLieferanten::on_pushButton_3_loeschen_clicked()
         qDebug() << "Fehler beim LÖSCHEN eines Lieferanten: " << query_delete.lastError().text();
         return;
     }
+}
+
+void OverviewLieferanten::on_pushButton_leeren_clicked()
+{
+    LeereForm();
+}
+
+void OverviewLieferanten::SpeichereLieferant(bool created)
+{
+}
+
+void OverviewLieferanten::LoescheLieferant()
+{
+}
+
+bool OverviewLieferanten::UeberpruefeEingabe()
+{
+    return false;
+}
+
+QString OverviewLieferanten::HolePLZVonOrt(int ort)
+{
+    return QString();
+}
+
+void OverviewLieferanten::WaehleLieferant(ListItem<lieferant> *lieferant)
+{
 }
--- overviewlieferanten.h
+++ overviewlieferanten.h
@@ -9,34 +9,58 @@
 
 using namespace std;
 
+// 
 namespace Ui {
 class OverviewLieferanten;
 }
 
+// Klasse für die Übersicht der Lieferanten
 class OverviewLieferanten : public QWidget
 {
     Q_OBJECT
 
+// Öffentliche Methoden
 public:
     explicit OverviewLieferanten(MainWindow* parent = nullptr);
     ~OverviewLieferanten();
 
+
+
+public slots:
+
+
+// Private Attribute    
 private slots:
     void on_list_item_clicked(ListItem<QString>* item);
-
     void on_back_to_main_clicked();
-
     void on_pushButton_anlegen_clicked();
-
     void on_pushButton_2_bearbeiten_clicked();
-
     void on_pushButton_3_loeschen_clicked();
+    void on_pushButton_leeren_clicked();
+
 
 private:
     Ui::OverviewLieferanten* ui;
     vector<ListItem<QString>*>* lieferanten;
     MainWindow* parent;
     QVBoxLayout* m_layout;
+
+    void LeereForm();
+    void LadeAnreden();
+    void LadeOrte();
+    void LadeLieferanten();
+    void LadeLieferanten(lieferant* lieferant);
+    void SpeichereLieferant(bool created);
+    void LoescheLieferant();
+    bool UeberpruefeEingabe();
+    QString HolePLZVonOrt(int ort);
+    ListItem<QString>* ErstelleLieferantItem(lieferant* lieferant);
+    void WaehleLieferant(ListItem<lieferanten>* lieferant);
+
+    ListItem<lieferant>* ErstelleLieferant(lieferant* lieferant);
+    ListItem<lieferant>* m_ausgewaelterLieferant;
+    vector<ListItem<lieferant>*>* m_lieferanten;
+
 };
 
 #endif // OVERVIEWLIEFERANTEN_H
